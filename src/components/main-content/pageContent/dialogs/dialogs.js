a998797680ac3f112cd437d4ff919ee4592c48e5
konenberga <konenber.a@gmail.com>
2021-09-27 22:32:49 +0300

    onClick homeWork

--- src/components/main-content/pageContent/dialogs/dialogs.js
+++ src/components/main-content/pageContent/dialogs/dialogs.js
@@ -11,14 +11,26 @@ const Dialogs = (props) => {
     let dialogs = props.dialogsPage.dialogsData.map(el => <Dialog name={el.name} id={el.id}/>);
     let messages = props.dialogsPage.messageData.map( el => <Message message={el.message}/>);
 
+    let textAreaElement = React.createRef()
+    let sendMessage = () => {
+        let text = textAreaElement.current.value
+        alert(text)
+    }
+
     return (
         <div>
             <div className={classes.chatRoom}>
                 <div className={classes.contactsWrapper}>
                     {dialogs}
                 </div>
-                <div className={classes.chat}>
-                    {messages}
+                <div>
+                    <div className={classes.chatMessage}>
+                        {messages}
+                    </div>
+                    <div className={classes.newsMessageInput}>
+                        <textarea ref={textAreaElement} placeholder={'type text here'}></textarea>
+                        <button  onClick={sendMessage}>SEND</button>
+                    </div>
                 </div>
             </div>
         </div>
