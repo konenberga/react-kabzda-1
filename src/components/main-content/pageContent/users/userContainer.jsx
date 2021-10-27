c13eb996f47712154415b2e958ee18ac0ceb489a
konenberga <konenber.a@gmail.com>
2021-10-27 11:01:32 +0300

    refactoring conteiner component - functional component

--- src/components/main-content/pageContent/users/userContainer.jsx
+++ src/components/main-content/pageContent/users/userContainer.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import {connect} from "react-redux";
-import UsersAPIComponent from "./usersAPIComponent";
 import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC} from "../../../../redux/usersReducer";
+import * as axios from "axios";
+import Users from "./users";
 
 
+class UsersComponent extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                    this.props.setUsers(response.data.items)
+                    this.props.setTotalUsersCount(response.data.totalCount)
+                }
+            )
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => this.props.setUsers(response.data.items))
+    }
+
+    render() {
+        return (
+            <Users totalUsersCount={this.props.totalUsersCount}
+                   pageSize={this.props.pageSize}
+                   onPageChanged={this.onPageChanged}
+                   currentPage={this.props.currentPage}
+                   users={this.props.users}
+                   follow={this.props.follow}
+                   unfollow={this.props.unfollow}/>
+        )
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -34,7 +64,6 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent)
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersComponent)
 
 export default UsersContainer
